d0203fc9f9b5ec12fe00ea9afa771042651c465b
stephhappens <stephanie.n.provence@gmail.com>
2016-11-27 12:46:46 -0600

    create/style homepage elements

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'sw-portfolio',
     environment: environment,
-    rootURL: '/indexs',
+    rootURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
